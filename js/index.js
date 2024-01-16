0c99ca14b2cbe47fa50ce2135b80ff4dd93f4226
marian brinza <branza.marian03@yahoo.com>
2024-01-16 19:01:45 +0200

    js to trigger animation when card is into view

--- js/index.js
+++ js/index.js
@@ -15,7 +15,6 @@ const effects = {
   crossed: 'crossed-off',
   bracket: 'bracket'
 };
-
 const colors = {
   green: 'hsl(159.4, 42.9%, 53.3%)',
   orange: 'hsl(37.1, 100%, 70.2%)',
@@ -35,13 +34,51 @@ const annotationConfig = {
   rtl: false
 }
 
-const usedEffects = [effects.underline, effects.box, effects.box, effects.box, effects.underline]
-const usedColors = [colors.green, colors.orange, colors.green, colors.orange, colors.red];
-const elementsToAnnotate = [exp, ux, endava, alten, drx];
+function applyAnnotationEffect(effects, colors, elements) {
+  elements.forEach((el, index) => {
+    annotationConfig.type = effects[index];
+    annotationConfig.color = colors[index];
+    const effect = annotate(el, annotationConfig);
+    effect.show();
+  });
+}
 
-elementsToAnnotate.forEach((element,index) => {
-  annotationConfig.type = usedEffects[index];
-  annotationConfig.color = usedColors[index];
-  const effect = annotate(element, annotationConfig);
-  effect.show();
+const headerEffects = [effects.underline, effects.box];
+const headerColors = [colors.green, colors.orange];
+const headerElements = [exp, ux];
+setTimeout(()=> applyAnnotationEffect(headerEffects, headerColors, headerElements), 400);
+
+
+const cardEffects = [effects.box, effects.box, effects.underline];
+const cardColors = [colors.green, colors.orange, colors.red];
+const cardElements = [endava, alten, drx];
+
+// scroll animation
+function handleIntersection(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      if (entry.target.classList.contains('card1') || entry.target.classList.contains('card3')) {
+        entry.target.classList.add('show');
+        entry.target.classList.add('slide-in-right');
+      } else {
+        entry.target.classList.add('show');
+        entry.target.classList.add('slide-in-left');
+      }
+
+      setTimeout(() => applyAnnotationEffect(cardEffects, cardColors, cardElements), 1000);
+
+      observer.unobserve(entry.target);
+    }
+  });
+}
+
+const observer = new IntersectionObserver(handleIntersection, {
+  root: null, // Use the viewport as the root
+  rootMargin: '0px', // No margin
+  threshold: 0.8, // Trigger when X% of the element is visible
+});
+
+const cards = document.querySelectorAll('.card1, .card2, .card3');
+cards.forEach((card) => {
+  observer.observe(card);
 });
